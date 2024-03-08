ffc2520a1f4db89a4db306a127157284acf872bd
Tys7603 <long.vlgt@gmail.com>
2024-03-08 13:10:00 +0700

    feat: play and pause song

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.musicapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MediaPlayer mediaPlayer;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize the MediaPlayer
+        mediaPlayer = MediaPlayer.create(this, R.raw.beat);
+
+       onPlayMusic();
+       onPauseMusic();
+
+    }
+
+    public void onPlayMusic(){
+        binding.play.setOnClickListener(view -> {
+            binding.play.setVisibility(View.GONE);
+            binding.stop.setVisibility(View.VISIBLE);
+            mediaPlayer.start();
+        });
+    }
+
+    public void onPauseMusic(){
+        binding.stop.setOnClickListener(view -> {
+            binding.stop.setVisibility(View.GONE);
+            binding.play.setVisibility(View.VISIBLE);
+            mediaPlayer.pause();
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
